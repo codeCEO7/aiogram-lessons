104d64740ac289fa7036ab057faf89fa498b201a
Kolini <kolini.dev@yandex.ru>
2022-04-17 15:26:34 +0300

    Обновлён .gitignore

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -5,5 +5,5 @@ from loader import dp
 
 @dp.message_handler(commands=['start'])
 async def command_start(message: types.Message):
-    await message.answer(f"Привет {message.from_user.first_name}",)
+    await message.answer(f"Привет {message.from_user.first_name}")
 
